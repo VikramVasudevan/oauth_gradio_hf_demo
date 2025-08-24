4497c62a763f05e97275ce8189cae40a209199c6
Vikram Vasudevan <vikram.vasudevan@ekahaa.com>
2025-08-24 07:04:08 +0530

    addressed small error in greet function

--- gradio_ui.py
+++ gradio_ui.py
@@ -7,10 +7,8 @@ def login_required_app():
     with gr.Blocks() as app:
         gr.Markdown("## This is a protected Gradio App!")
         name_input = gr.Textbox(label="Your Name")
-        output = gr.Textbox(label="Greeting")
-        name_input.submit(fn=greet, inputs=name_input, outputs=output)
         logout_btn = gr.Button("Logout", link="/logout")
-        gr.on(fn=greet, inputs=None, outputs=name_input)
+        gr.on(fn=greet, outputs=name_input)
     return app
 
 
